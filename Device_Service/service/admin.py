bc08bc38bc7c6314cdbee1d9ee12ebb390a740c8
salavatgibadullin <salavat9154@gmail.com>
2025-04-11 16:04:32 +0500

    изменение миграций

--- Device_Service/service/admin.py
+++ Device_Service/service/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Brand, Device
+from .models import Brand, DeviceType, Device_model, Feedback
 
+admin.site.register(DeviceType)
 admin.site.register(Brand)
-admin.site.register(Device)
+admin.site.register(Device_model)
+admin.site.register(Feedback)
--- Device_Service/service/models.py
+++ Device_Service/service/models.py
@@ -60,19 +60,18 @@ class Feedback(models.Model):
 #     def __str__(self):
 #         return f"{self.get_type_display()} {self.brand.name} {self.model_name}"
 
-
-"""
-Реализация формы обратной связи
-"""
-class Feedback(models.Model):
-    name = models.CharField(max_length=50) #имя
-    phone = models.DecimalField(max_digits=10, decimal_places=0) #телефон
-    message = models.TextField() #сообщение
-    # created_at = models.DateTimeField(auto_now_add=True) #дата создания
-    reply = models.TextField(blank=True, null=True) #ответ на сообщение
-    replied = models.BooleanField(default=False) #отвечено или нет
-
-    def __str__(self):
-        return f"{self.name} ({'отвечено' if self.replied else 'ожидает ответа'})"
-
+#
+# """
+# Реализация формы обратной связи
+# """
+# class Feedback(models.Model):
+#     name = models.CharField(max_length=50) #имя
+#     phone = models.DecimalField(max_digits=10, decimal_places=0) #телефон
+#     message = models.TextField() #сообщение
+#     # created_at = models.DateTimeField(auto_now_add=True) #дата создания
+#     reply = models.TextField(blank=True, null=True) #ответ на сообщение
+#     replied = models.BooleanField(default=False) #отвечено или нет
+#
+#     def __str__(self):
+#         return f"{self.name} ({'отвечено' if self.replied else 'ожидает ответа'})"
 
