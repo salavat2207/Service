ee90c69eb8d48cefb6c1e67b1625b3dfc5f62408
salavatgibadullin <salavat9154@gmail.com>
2025-04-11 15:45:46 +0500

    изменение models.py добавление классов

--- Device_Service/service/models.py
+++ Device_Service/service/models.py
@@ -2,32 +2,65 @@ from datetime import date
 
 from django.db import models
 
+# DeviceType - тип устройства (смартфон, ноутбук и т.д.)
+class DeviceType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+# Brand - производитель (Apple, Samsung, Xiaomi и т.д.)
 class Brand(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=50, unique=True)
 
-    def __str__(self):
-        return self.name
-
+# Device — конкретное устройство клиента
 class Device(models.Model):
-    DEVICE_TYPES = [
-        ('apple', 'Apple'),
-        ('samsung', 'Samsung'),
-        ('xiaomi', 'Xiaomi'),
-        ('dyson', 'Dyson'),
-        ('vacuum_cleaner', 'Пылесосы'),
-        ('console', 'Игровые консоли'),
-    ]
-
-    type = models.CharField(max_length=50, choices=DEVICE_TYPES)  #тип модели
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='devices') #бренд
-    model_name = models.CharField(max_length=50) #модель
-    repair_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) #стоимость ремонта
-    repair_status = models.TextField(blank=True, null=True) #статус ремонта
-    diagnostics_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0) #диагностика
+    type = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    model = models.CharField(max_length=50)
 
 
-    def __str__(self):
-        return f"{self.get_type_display()} {self.brand.name} {self.model_name}"
+# Feedback - форма обратной связи
+class Feedback(models.Model):
+    # name = models.CharField(max_length=50) #имя
+    phone = models.DecimalField(max_digits=10, decimal_places=0) #телефон
+    message = models.TextField() #сообщение
+
+    # ниже строки под вопросом добавления
+
+    # created_at = models.DateTimeField(auto_now_add=True) #дата создания
+    # reply = models.TextField(blank=True, null=True) #ответ на сообщение
+    # replied = models.BooleanField(default=False) #отвечено или нет
+
+
+
+
+
+
+
+
+
+
+# старый код
+
+
+# class Device(models.Model):
+#     DEVICE_TYPES = [
+#         ('apple', 'Apple'),
+#         ('samsung', 'Samsung'),
+#         ('xiaomi', 'Xiaomi'),
+#         ('dyson', 'Dyson'),
+#         ('vacuum_cleaner', 'Пылесосы'),
+#         ('console', 'Игровые консоли'),
+#     ]
+#
+#     type = models.CharField(max_length=50, choices=DEVICE_TYPES)  #тип модели
+#     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='devices') #бренд
+#     model_name = models.CharField(max_length=50) #модель
+#     repair_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) #стоимость ремонта
+#     repair_status = models.TextField(blank=True, null=True) #статус ремонта
+#     diagnostics_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0) #диагностика
+#
+#
+#     def __str__(self):
+#         return f"{self.get_type_display()} {self.brand.name} {self.model_name}"
 
 
 """
